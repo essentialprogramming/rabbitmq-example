bc311ec6ec56ec37beeb46d0c808d8bea7f249f9
Mindru Mircea <mircea.mindru@avangarde-software.com>
2022-02-07 16:27:13 +0200

    #1630 - add and configure log4j2.

--- spring-boot-rabbitmq-producer/src/main/java/com/rabbitmq/config/RabbitConfig.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/rabbitmq/config/RabbitConfig.java
@@ -1,6 +1,9 @@
 package com.rabbitmq.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -12,40 +15,31 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     public final String QUEUE = "queue";
-    public final String EXCHANGEDIRECT = "exchangeDirect";
-    public final String ROUTINGA = "routingA";
+    public final String EXCHANGE_DIRECT = "exchangeDirect";
+    public final String ROUTING_A = "routingA";
 
     @Bean
-    Queue queueA(){
-        return new Queue(QUEUE,false);
+    Queue queueA() {
+        return new Queue(QUEUE, false);
     }
 
     @Bean
-    DirectExchange exchange(){
-        return new DirectExchange(EXCHANGEDIRECT);
+    DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_DIRECT);
     }
 
     @Bean
-    Binding binding(Queue queueA, DirectExchange exchange){
-        return  BindingBuilder.bind(queueA).to(exchange).with(ROUTINGA);
+    Binding binding(Queue queueA, DirectExchange exchange) {
+        return BindingBuilder.bind(queueA).to(exchange).with(ROUTING_A);
     }
 
-
     @Bean
-    public MessageConverter converter(){
+    public MessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
 
-//    @Bean
-//    public ConnectionFactory connectionFactory() {
-//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
-//        connectionFactory.setUsername("guest");
-//        connectionFactory.setPassword("guest");
-//        return connectionFactory;
-//    }
-
     @Bean
-    public RabbitTemplate template(ConnectionFactory connectionFactory){
+    public RabbitTemplate template(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(converter());
         return rabbitTemplate;
--- spring-boot-rabbitmq-producer/src/main/java/com/rabbitmq/controller/Producer.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/rabbitmq/controller/Producer.java
@@ -1,6 +1,7 @@
 package com.rabbitmq.controller;
 
 import com.rabbitmq.model.Msg;
+import org.slf4j.Logger;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 public class Producer {
+
+    private static final Logger LOG = getLogger(Producer.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -19,8 +24,8 @@ public class Producer {
 
     @PostMapping("/post")
     public String send(@RequestBody Msg msg) {
-
-        rabbitTemplate.convertAndSend(directExchange.getName(),"routingA",msg);
+        LOG.info("Sending message={} to exchange={}", msg, directExchange.getName());
+        rabbitTemplate.convertAndSend(directExchange.getName(), "routingA", msg);
         return msg.toString();
     }
 }
