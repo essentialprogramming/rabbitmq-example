a049c072f8bcddc26ff2737efb97cb198892a093
Mindru Mircea <mircea.mindru@avangarde-software.com>
2022-02-07 13:25:27 +0200

    Add Spring Boot RabbitMQ producer app.

---
+++ spring-boot-rabbitmq-producer/src/test/java/com/avangarde/rabbitmq/springbootrabbitmqproducer/SpringBootRabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avangarde.rabbitmq.springbootrabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
