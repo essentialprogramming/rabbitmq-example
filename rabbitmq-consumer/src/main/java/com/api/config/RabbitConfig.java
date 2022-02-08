c9b5586daa21dea4b6b8c09a9bcb946ee82da1ea
DESKTOP-PE9O5RC\Catalina Suilea <catalinasuilea@yahoo.com>
2022-02-08 15:10:38 +0200

    Add Fanout Exchange for producer/consumer.

--- rabbitmq-consumer/src/main/java/com/api/config/RabbitConfig.java
+++ rabbitmq-consumer/src/main/java/com/api/config/RabbitConfig.java
@@ -1,8 +1,8 @@
 package com.api.config;
 
 import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -15,13 +15,17 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.springframework.amqp.core.BindingBuilder.bind;
+
 @EnableRabbit
 @Configuration
 public class RabbitConfig {
 
     public final String QUEUE = "queue";
-    public final String EXCHANGEDIRECT = "exchangeDirect";
-    public final String ROUTINGA = "routingA";
+    public final String FANOUT_QUEUE = "fanoutQueue";
+    public final String EXCHANGE_DIRECT = "exchangeDirect";
+    public final String EXCHANGE_FANOUT = "exchangeFanout";
+    public final String ROUTING_A = "routingA";
 
     @Bean
     Queue queueA(){
@@ -29,13 +33,28 @@ public class RabbitConfig {
     }
 
     @Bean
-    DirectExchange exchange(){
-        return new DirectExchange(EXCHANGEDIRECT);
+    Queue queueFanout() {
+        return new Queue(FANOUT_QUEUE, false);
     }
 
     @Bean
-    Binding binding(Queue queueA, DirectExchange exchange){
-        return  BindingBuilder.bind(queueA).to(exchange).with(ROUTINGA);
+    DirectExchange directExchange() {
+        return new DirectExchange(EXCHANGE_DIRECT);
+    }
+
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange(EXCHANGE_FANOUT);
+    }
+
+    @Bean
+    Binding binding(DirectExchange exchange) {
+        return bind(queueA()).to(exchange).with(ROUTING_A);
+    }
+
+    @Bean
+    Binding bindingFanout(FanoutExchange exchange) {
+        return bind(queueFanout()).to(exchange);
     }
 
 
